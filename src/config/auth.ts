2769a98ecdb0cacaf8d955f6ff6bcc752e87c465
TimmyKuo <taguo1109@gmail.com>
2025-10-04 18:35:09 +0800

    20251004 url change By Timmy

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,5 +1,5 @@
-export const FRONTEND_BASE = 'http://localhost:5173/renthouse';
-export const OAUTH_BASE = 'http://localhost:8080';
+export const FRONTEND_BASE = 'https://taguo1109.github.io/renthouse';
+export const OAUTH_BASE = 'https://oauth.zeabur.app';
 
 export const GOOGLE_LOGIN_URL =
   `${OAUTH_BASE}/auth/entry?target=${encodeURIComponent(FRONTEND_BASE + '/profile')}`;
