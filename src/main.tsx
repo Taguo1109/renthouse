c4988de123dd5ec5cd65960a552984f5d1095bea
TimmyKuo <taguo1109@gmail.com>
2025-10-04 19:13:53 +0800

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { HelmetProvider } from 'react-helmet-async';
-import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
-      <HashRouter>
         <App />
-      </HashRouter>
     </HelmetProvider>
   </StrictMode>
 );
